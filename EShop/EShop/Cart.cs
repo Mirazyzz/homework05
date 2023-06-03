61ff49f42a74187bb9e5b4c92d4b1f7193d1723e
MuhammadaliUsa <128176737+MuhammadaliUsa@users.noreply.github.com>
2023-06-03 15:43:23 +0500

    Add files via upload

---
+++ EShop/EShop/Cart.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EShop
+{
+    internal class Cart
+    {
+        public int Id { get; set; }
+        Dictionary<Product, int> quentity;
+        public int CustomerId { get; set; }
+        List<string> products = new List<string>();
+
+        public void AddProduct()
+        {
+
+
+        }
+        public void RemoveProduct()
+        {
+
+        }
+        public void CalculateProduct()
+        {
+
+        }
+    }
+}
